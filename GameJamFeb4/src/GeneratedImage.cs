68414a77681a03873e82187854fb0ef8f1f610b8
yaouDev <augustjansson@outlook.com>
2023-04-25 13:11:27 +0200

    updated v1

--- GameJamFeb4/src/GeneratedImage.cs
+++ GameJamFeb4/src/GeneratedImage.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.IO;
 
 namespace GameJamFeb4.src
 {
@@ -31,6 +32,11 @@ namespace GameJamFeb4.src
         public readonly string fileName;
         public readonly int priorityStrength;
 
+        public GeneratedImage(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
         public GeneratedImage(string fileName, Color[] colors, int priorityStrength) //colors ranked in order of priority (0 being lowest);
         {
             this.fileName = fileName;
@@ -38,17 +44,23 @@ namespace GameJamFeb4.src
             this.priorityStrength = priorityStrength;
         }
 
-        public Bitmap Generate(int width, int height, int timesToIterate)
+        public Bitmap Generate(int width, int height, int timesToIterate, IterationMethod method)
         {
             image = new Bitmap(width, height);
             iterations = 0;
 
             Fill(image, baseColor);
             Draw(image);
-            Iterate(timesToIterate, 100, false);
+            Iterate(timesToIterate, 100, false, method);
 
             image.Save(fileName, ImageFormat.Png);
             return image;
+        }
+
+        public void Insert(Image temp)
+        {
+            image = (Bitmap)temp;
+            temp.Save(fileName, ImageFormat.Bmp);
         }
 
         private void Draw(Bitmap bmp) //O(N * M * O)...
@@ -71,8 +83,46 @@ namespace GameJamFeb4.src
             existingColors.Keys.ToList().ForEach(key => Console.WriteLine(key + ": " + existingColors[key]));
         }
 
-        public Bitmap Iterate(int timesToIterate, int oddsToBeUnchanged, bool save)
+        private Vector?[] ScrambleBmp()
         {
+            Vector?[] result = new Vector?[image.Width * image.Height];
+            Random rnd = new Random();
+
+            for(int x = 0; x < image.Width; x++)
+            {
+                for(int y = 0; y < image.Height; y++)
+                {
+                    int index = rnd.Next(0, result.Length);
+                    while (result[index] != null)
+                    {
+                        index++;
+                        if (index > result.Length - 1) index %= result.Length;
+                    }
+
+                    result[index] = new Vector(x, y);
+                }
+            }
+
+            return result;
+        }
+
+        public Bitmap Iterate(int timesToIterate, int oddsToBeUnchanged, bool save, IterationMethod method)
+        {
+            switch (method)
+            {
+                case IterationMethod.ordered:
+                    return IterateOrdered(timesToIterate, oddsToBeUnchanged, save);
+                case IterationMethod.scrambled:
+                    return IterateScrambled(timesToIterate, oddsToBeUnchanged, save);
+                default:
+                    return IterateOrdered(timesToIterate, oddsToBeUnchanged, save);
+            }
+        }
+        
+        public Bitmap IterateOrdered(int timesToIterate, int oddsToBeUnchanged, bool save, int matrixSize = 3) //odds lower effectivity of iteration
+        {
+            if (image == null) throw new NullReferenceException("Image is null.");
+
             for (int i = 0; i < timesToIterate; i++)
             {
                 Dictionary<Color, int> existingColors = new Dictionary<Color, int>();
@@ -89,7 +139,7 @@ namespace GameJamFeb4.src
                         }
                         else
                         {
-                            Color[] neighbors = GetMatrix(x, y, image, 3);
+                            Color[] neighbors = GetMatrix(x, y, image, matrixSize);
                             Dictionary<Color, int> occurance = new Dictionary<Color, int>();
 
                             foreach (Color neighbor in neighbors)
@@ -122,6 +172,58 @@ namespace GameJamFeb4.src
         }
 
         
+        public Bitmap IterateScrambled(int timesToIterate, int oddsToBeUnchanged, bool save, int matrixSize = 3) //odds lower effectivity of iteration
+        {
+            if (image == null) throw new NullReferenceException("Image is null.");
+
+            Vector?[] pixels = ScrambleBmp();
+
+            for (int i = 0; i < timesToIterate; i++)
+            {
+                Dictionary<Color, int> existingColors = new Dictionary<Color, int>();
+                iterations++;
+
+                foreach (Vector v in pixels)
+                {
+                    Color currentColor = Color.Empty;
+
+                    if (calc.YesOrNo(oddsToBeUnchanged))
+                    {
+                        currentColor = image.GetPixel(v.x, v.y);
+                    }
+                    else
+                    {
+                        Color[] neighbors = GetMatrix(v.x, v.y, image, matrixSize);
+                        Dictionary<Color, int> occurance = new Dictionary<Color, int>();
+
+                        foreach (Color neighbor in neighbors)
+                        {
+                            if (occurance.ContainsKey(neighbor)) occurance[neighbor]++;
+                            else occurance.Add(neighbor, 1);
+                        }
+
+                        currentColor = occurance.Aggregate((a, b) => a.Value > b.Value ? a : b).Key;
+
+                        while (currentColor.IsEmpty)
+                        {
+                            currentColor = neighbors[calc.PickRndIndex(neighbors)];
+                        }
+                    }
+
+                    image.SetPixel(v.x, v.y, currentColor);
+
+                    if (existingColors.ContainsKey(currentColor)) existingColors[currentColor]++;
+                    else existingColors.Add(currentColor, 1);
+                }
+
+                Console.WriteLine("Iteration " + iterations + " Color Result: ");
+                existingColors.Keys.ToList().ForEach(key => Console.WriteLine(key + ": " + existingColors[key]));
+            }
+
+            if (save) image.Save(fileName, ImageFormat.Png);
+
+            return image;
+        }
 
         private void Fill(Bitmap bmp, Color color)
         {
@@ -164,4 +266,19 @@ namespace GameJamFeb4.src
             return matrix;
         }
     }
+
+    public struct Vector
+    {
+        public readonly int x, y;
+        public Vector(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
+public enum IterationMethod
+{
+    ordered, scrambled
 }
--- GameJamFeb4/src/Program.cs
+++ GameJamFeb4/src/Program.cs
@@ -7,11 +7,13 @@ namespace GameJamFeb4.src
     {
         static void Main(string[] args)
         {
-            //Color[] colors = { Color.Black, Color.Red, Color.Orange, Color.Yellow, Color.Blue, Color.Cyan, Color.White };
-            Color[] colors = { Color.Black, Color.White };
-            GeneratedImage g = new GeneratedImage("generated", colors, 100);
+            /*
+            Color[] colors = { Color.Black, Color.Red, Color.Orange, Color.Yellow, Color.Blue, Color.Cyan, Color.White };
+            //Color[] colors = { Color.Black, Color.White };
+            GeneratedImage g = new GeneratedImage("generated", colors, 4);
 
-            Bitmap testmap = g.Generate(100, 100, 5);
+
+            Bitmap testmap = g.Generate(100, 100, 50, IterationMethod.scrambled);*/
 
             //g.Iterate(4, true);
             /*Color[] nine = g.GetMatrix(-1, -1, testmap, 3);
@@ -23,6 +25,15 @@ namespace GameJamFeb4.src
             ProbabilityCalculator<int> calc = new ProbabilityCalculator<int>();
             int[] ints = { 1, 2, 3, 4, 5 };
             Console.WriteLine(calc.Calculate(ints, 10));*/
+
+            
+            Image test = Image.FromFile("insert.jpg");
+            GeneratedImage g = new GeneratedImage("generated");
+
+            g.Insert(test);
+            //g.Iterate(15, 100, true, IterationMethod.scrambled);
+            //g.IterateOrdered(15, 100, true, 7);
+            g.IterateScrambled(15, 100, true, 7);
         }
     }
 }
